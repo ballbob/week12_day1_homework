b3147d424945b1b6ff28b1314ab1f43d902914e3
Robert Ball <robert.ball.1989@gmail.com>
2017-04-17 18:01:57 +0100

    Commented through the server.js file, added body parser requirements. Created controllers directory and files.

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+//make bodyParser
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
 var path = require('path')
 
+
+//specifies the html page for the webpage
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
+//specify the directories for the express object to use
 app.use(express.static('client/build'));
+app.use(require('./client/controllers'))
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
+//specifies the port/address to use for the website
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
